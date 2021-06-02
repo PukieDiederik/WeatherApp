f312f3513c3c4eb7dbc82b8a8e3207c667e308f0
pukiediederik <diederik.boeren@gmail.com>
2021-06-02 08:30:49 +0200

    ADD: function for calling onecall api

--- weather.py
+++ weather.py
@@ -1,21 +1,12 @@
+# %%
 import geocoder
 import requests
-import datetime
 
-import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-WEATHER_BASEURL = "http://api.openweathermap.org/data/2.5"
-WEATHER_ONECALL_ENDPOINT = "/onecall" #options:  lat={lat}& lon={lon} & exclude={part} & appid={API key}
-
-g = geocoder.ip("me").latlng
-
-
-
-data = requests.get(WEATHER_BASEURL + WEATHER_ONECALL_ENDPOINT + "?lat={}&lon={}&appid={}".format(g[0],g[1],os.environ.get("WEATHER_APIKEY")))
-
-print(g)
-print(data.json())
-print(os.environ.get("WEATHER_APIKEY"))
+# %% Call openWeatherMap's onecall api
+# exclude should be a string with each section seperated by a comma (no spaces)
+def oneCallRequest(apiKey : str, latlng, exclude):
+    data = requests.get(f"http://api.openweathermap.org/data/2.5/onecall?lat={latlng[0]}&lon={latlng[1]}&exclude={exclude}&appid={apiKey}").json()
+    return data
